b30122ae50d4bd35a430ff45d7ac266413298d79
Ali Stone <a.stone1794@gmail.com>
2018-07-18 16:57:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ function happyHolidaysTo(name) {
 }
 
 function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(name, numberOfDays) {
   return 
 }
